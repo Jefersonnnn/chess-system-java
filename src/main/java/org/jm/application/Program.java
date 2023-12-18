70af4794f22b29d811e7cb1f0e6ac0c43f83a569
Jefersonnnn <jeferson_m94@hotmail.com>
2023-12-18 14:52:47 -0300

    Starting to implement Board and Piece

--- src/main/java/org/jm/application/Program.java
+++ src/main/java/org/jm/application/Program.java
@@ -1,10 +1,10 @@
 package org.jm.application;
 
-import org.jm.boardgame.Position;
+import org.jm.boardgame.Board;
 
 public class Program {
     public static void main(String[] args) {
-        Position pos = new Position(3,5);
-        System.out.println(pos);
+
+        Board board = new Board(8,8);
     }
 }
