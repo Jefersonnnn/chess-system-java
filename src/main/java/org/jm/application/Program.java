f8fcf5deb0b3275b2b672db615074303fec090de
Jefersonnnn <jeferson_m94@hotmail.com>
2023-12-18 15:11:37 -0300

    Chess layer and printing the board

--- src/main/java/org/jm/application/Program.java
+++ src/main/java/org/jm/application/Program.java
@@ -1,10 +1,12 @@
 package org.jm.application;
 
 import org.jm.boardgame.Board;
+import org.jm.chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
 
-        Board board = new Board(8,8);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
---
+++ src/main/java/org/jm/chess/ChessPiece.java
@@ -0,0 +1,18 @@
+package org.jm.chess;
+
+import org.jm.boardgame.Board;
+import org.jm.boardgame.Piece;
+
+public class ChessPiece extends Piece {
+    private Color color;
+
+    public ChessPiece(Board board, Color color) {
+        super(board);
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+}
